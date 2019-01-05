f195e44c565328ffcfeee732bfbbfa0962c3ad1a
tkotosz <tkotosz@inviqa.com>
2019-01-05 14:28:48 +0100

    add missing composer json

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar -cf init.tar Dockerfile func.init.yaml func.php
+tar -cf init.tar Dockerfile func.init.yaml composer.json func.php
 docker build -t tkotosz/fn-php-init -f Dockerfile-init-image .
